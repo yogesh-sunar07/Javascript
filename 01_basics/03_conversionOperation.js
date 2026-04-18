6b197bfa226c769ebf5b548edd9c69afff3bd271
yogesh-sunar07 <yogesu07@gmail.com>
2026-04-18 17:07:06 +0545

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "3adc"
+console.log(typeof score )
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggesIn = 1
+let booleanIsLoggedIn = Boolean(isloggesIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0=> false
+// "" => false
+// "yogesh" =>  true
+
+
+let someNumber  = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log( stringNumber);

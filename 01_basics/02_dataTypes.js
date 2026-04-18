a02138e6b490c18d6fc9ad55e681237779331a25
yogesh-sunar07 <yogesu07@gmail.com>
2026-04-18 08:25:25 +0545

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" ;//treat all js code as newer verson
+// alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3) 
+console.log("yogesh")
+
+let name = "yogesh"
+let age = 21
+let isLOggedIn = false
+
+
+//premetive data type 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boollean => true/false
+// null => standalone value
+// undefined => 
+// symbol =>  unique
+
+
+// object
+
+console.log( typeof age)
+console.log( typeof "yogesh")
+console.log( typeof null)
+console.log( typeof undefined)
